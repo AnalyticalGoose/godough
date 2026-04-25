5b7e6537b035c7f991656d40876b1a9071deaa3a
AnalyticalGoose <95051021+AnalyticalGoose@users.noreply.github.com>
2026-04-25 10:01:14 +0100

    Add prep for 3D Editor
    Add camera nodes, grid shader, renderer window scenes

--- ui/gizmos/viewport_orientation/orientation_gizmo_overlay.gd
+++ ui/gizmos/viewport_orientation/orientation_gizmo_overlay.gd
@@ -3,5 +3,5 @@ extends TextureRect
 @export var orientation_gizmo_viewport: SubViewport
 
 func _on_gui_input(event: InputEvent) -> void:
-    orientation_gizmo_viewport.notification(NOTIFICATION_VP_MOUSE_ENTER)
-    orientation_gizmo_viewport.push_input(event)
+	orientation_gizmo_viewport.notification(NOTIFICATION_VP_MOUSE_ENTER)
+	orientation_gizmo_viewport.push_input(event)
--- ui/renderer_window.gd
+++ ui/renderer_window.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_viewport_orientation_gizmo_gizmo_clicked(idx: int) -> void:
-    print(idx)
+	print(idx)
